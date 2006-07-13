a01897d24294d14965e9fafd5490af6d6cb2ac65
mosman <mosman>
2006-07-13 14:22:24 +0000

    no message

---
+++ MSmarty.class.php
@@ -0,0 +1,27 @@
+<?
+require_once("Smarty.class.php");
+
+
+class MSmarty extends Smarty {
+
+    function _read_file($filename)
+    {
+        $res = false;
+
+        if (file_exists($filename)) {
+            if (function_exists('ioncube_read_file')) {
+                $res = ioncube_read_file($filename);
+                if (is_int($res)) $res = false;
+            }
+            else if ( ($fd = @fopen($filename, 'rb')) ) {
+                $res = ($size = filesize($filename)) ? fread($fd, $size) : '';
+                fclose($fd);
+            }
+        }
+
+        return $res;
+    }
+
+}
+
+?>
